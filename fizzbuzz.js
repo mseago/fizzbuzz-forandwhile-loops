1ecad9bb8397eb9464eaec4d0bcbf3be1847c27f
Melissa Seago <melissa.seago@gmail.com>
2016-05-29 18:17:50 -0500

    updated to incl function wraparound

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,33 +1,36 @@
+function fizzBuzzForLoop () {
+  for (var i = 1; i <= 30; i++) {
 
-for (var i = 1; i <= 30; i++) {
-
-  if (i % 3 === 0 && i % 5 === 0) {
-    return "FizzBuzz";
-  }
-  else if (i % 3 === 0) {
-    return "Fizz";
-  }
-  else if (i % 5 === 0) {
-    return "Buzz";
-  }
-  else {
-    return i;
+    if (i % 3 === 0 && i % 5 === 0) {
+      return "FizzBuzz";
+    }
+    else if (i % 3 === 0) {
+      return "Fizz";
+    }
+    else if (i % 5 === 0) {
+      return "Buzz";
+    }
+    else {
+      return i;
+    }
   }
 }
 
-var i = 1;
-while (i <= 30) {
-  i++;
-  if (i % 3 === 0 && i % 5 === 0) {
-    return "FizzBuzz";
-  }
-  else if (i % 3 === 0) {
-    return "Fizz";
-  }
-  else if (i % 5 === 0) {
-    return "Buzz";
-  }
-  else {
-    return i;
+function fizzBuzzWhileLoop () {
+  var i = 1;
+  while (i <= 30) {
+    i++;
+    if (i % 3 === 0 && i % 5 === 0) {
+      return "FizzBuzz";
+    }
+    else if (i % 3 === 0) {
+      return "Fizz";
+    }
+    else if (i % 5 === 0) {
+      return "Buzz";
+    }
+    else {
+      return i;
+    }
   }
 }
