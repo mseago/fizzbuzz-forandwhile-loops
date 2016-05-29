49d9fc6c2f373059b6a8c4177a7a9415be973d42
Melissa Seago <melissa.seago@gmail.com>
2016-05-29 18:10:06 -0500

    first commit

---
+++ fizzbuzz.js
@@ -0,0 +1,33 @@
+
+for (var i = 1; i <= 30; i++) {
+
+  if (i % 3 === 0 && i % 5 === 0) {
+    return "FizzBuzz";
+  }
+  else if (i % 3 === 0) {
+    return "Fizz";
+  }
+  else if (i % 5 === 0) {
+    return "Buzz";
+  }
+  else {
+    return i;
+  }
+}
+
+var i = 1;
+while (i <= 30) {
+  i++;
+  if (i % 3 === 0 && i % 5 === 0) {
+    return "FizzBuzz";
+  }
+  else if (i % 3 === 0) {
+    return "Fizz";
+  }
+  else if (i % 5 === 0) {
+    return "Buzz";
+  }
+  else {
+    return i;
+  }
+}
